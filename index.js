3a3a5ef47625ca4d9ab3337b4ad04622c0d891f6
yaoqufeng <78394060+yaoqufeng@users.noreply.github.com>
2025-09-24 16:22:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,21 +12,22 @@ module.exports = async ({ req, res, log }) => {
   const { execSync } = require('child_process');
 
   // Configuration variables
-  const UPLOAD_URL = '';
-  const PROJECT_URL = '';
-  const AUTO_ACCESS = true;
-  const FILE_PATH = '/tmp';
-  const SUB_PATH = 'sub';
-  const UUID = '5d5bc519-4996-495d-8d01-65730f4fa49f';
-  const NEZHA_SERVER = 'ip.086571.xyz:8008';
-  const NEZHA_PORT = '';
-  const NEZHA_KEY = 'YiSBm5Hzzl9AadRQjLcHymN2WoZTR6hk';
-  const ARGO_DOMAIN = 'appwrite.086571.xyz';
-  const ARGO_AUTH = 'eyJhIjoiNTdhZDk5Y2U4MDQ2MTg2ZGEyZWI1ZTQ3YjVmODlhNDMiLCJ0IjoiMDUzOWFmZGEtYWQ1Mi00YTU0LTg1MDQtNTI1ZGZiZDI5YWQ3IiwicyI6Ik1EY3pPVEl6TnpBdFpXWmpNeTAwTXpNeUxUZzVaall0TXprMk4yVXpPREkwTlRRNCJ9';
-  const ARGO_PORT = 8001;
-  const CFIP = 'www.visa.com.hk';
-  const CFPORT = 443;
-  const NAME = 'Appwrite';
+const UPLOAD_URL = process.env.UPLOAD_URL || '';      // 节点或订阅自动上传地址,需填写部署Merge-sub项目后的首页地址,例如：https://merge.serv00.net
+const PROJECT_URL = process.env.PROJECT_URL || '';    // 需要上传订阅或保活时需填写项目分配的url,例如：https://google.com
+const AUTO_ACCESS = process.env.AUTO_ACCESS || true; // false关闭自动保活，true开启,需同时填写PROJECT_URL变量
+const FILE_PATH = process.env.FILE_PATH || './tmp';   // 运行目录,sub节点文件保存目录
+const SUB_PATH = process.env.SUB_PATH || 'sub';       // 订阅路径
+const UUID = process.env.UUID || '5d5bc519-4996-495d-8d01-65730f4fa49f'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
+const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
+const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
+const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://fscarmen.cloudflare.now.cc
+const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
+const CFIP = process.env.CFIP || 'www.visa.com.hk';         // 节点优选域名或优选ip  
+const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
+const NAME = process.env.NAME || 'Appwrite';                     // 节点名称
+
 
 if (!fs.existsSync(FILE_PATH)) {
   fs.mkdirSync(FILE_PATH);
@@ -614,4 +615,5 @@ async function AddVisitTask() {
     }, 500);
   }
 };
+
 
